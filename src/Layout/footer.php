916a70b83b6a69d3646fafb0b2f9afa3b9fe5e41
DavLin <davlin@creatorise.com>
2021-12-09 14:49:35 +0100

    Fix source links to use relative path

--- src/Layout/footer.php
+++ src/Layout/footer.php
@@ -1,6 +1,6 @@
     <footer>
       <p><i>Placeholder text for footer.</i> Terraphiliacs 2021</p>
     </footer>
-    <script src="../../script.js"></script>
+    <script src="../../public/js/script.js"></script>
   </body>
 </html>
--- src/Layout/header.php
+++ src/Layout/header.php
@@ -4,11 +4,11 @@
       <nav>
         <span class="menuButton">&equiv;</span>
         <div class="home">
-          <img class="logo" src="https://halvkursprojekt-webbutv.manfredgustafss.repl.co/images/pine.png" alt=" ">
+          <img class="logo" src="../../images/pine.png" alt=" ">
           <h1>Terraphiliacs</h1>
         </div>
         <div class="menu mobileHidden noTransition">
-          <a href="https://halvkursprojekt-webbutv.manfredgustafss.repl.co/articles/">Articles</a>
+          <a href="../../articles/">Articles</a>
           <a href="#">Test2</a>
           <a href="#">Test3</a>
         </div>
