30ad70f204c6632fe1c017e123b577afc75dc475
DavLin <davlin@creatorise.com>
2021-12-09 11:27:30 +0100

    Refactor main tag

---
+++ php-files/main.php
@@ -0,0 +1,45 @@
+<main>
+    <div class="card">
+    
+    <p>
+
+        Eat trash, burn trees, consume thy parents, go hunting for prey, drink all the water, no more water, fish can't swim on land, other people can't live without water, you hold all the water, they're your slaves, create an empire,
+    </p>
+    <p>
+
+        AGE OF EMPIRES III
+    </p>
+
+    <p>
+
+        enter a tornado, slay your enemies, you're in the colusseum. Hira a scientist, make the scientist clonse more scientist, make these scientist develop you. You're a product of scientist, sell yourself on the black market. Grind Money. Go back to the colusseum. Kill people. Gather their body parts, sell their body parts on the black market.
+        Buy a paper factory, eat the paper! You're the paper. sell the paper, sell youself. I'm a sellout.
+    </p>
+
+    <p> / quote by "Ed Sherran"
+    </div>
+
+    <img src="https://images.unsplash.com/photo-1507783548227-544c3b8fc065" tabindex=-1>
+
+    <div class="card">
+    <h1>
+        <span>kjfhaksjdhf</span>
+    </h1>
+    <p>
+
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptatibus, dolorum tenetur voluptates quaerat modi minima id ut, non laborum nam odit. Nostrum voluptates dignissimos, corrupti voluptatem hic blanditiis laboriosam.
+    Perferendis mollitia est quod minus! Optio quia dolorem, voluptas modi animi laborum iusto, explicabo, qui quos atque debitis quisquam eaque. Laudantium facilis iste quaerat id et aspernatur mollitia magnam inventore.
+    Distinctio voluptatibus, dignissimos quam nemo blanditiis aut soluta modi qui sunt ut eveniet officiis dolorum voluptas obcaecati deserunt quia saepe vel nihil mollitia culpa tempora alias consequatur illum! Iusto, quod?
+    Dolore sit vitae quam in repellat officiis perspiciatis praesentium aut, fugiat rem reprehenderit amet autem sed distinctio! Adipisci temporibus ab vitae veniam reiciendis, ut quo repellat explicabo, nobis ex perspiciatis!
+    Sit numquam iusto tempora cumque et labore, expedita mollitia illum incidunt libero eaque, perferendis atque cupiditate, nostrum non itaque esse! Esse, ex. Alias asperiores accusamus inventore consequatur quaerat non ad.
+    Ad eaque, saepe vero cumque suscipit accusantium a molestiae non blanditiis eum neque facilis, corporis magnam amet quidem delectus, ipsum quibusdam beatae repudiandae aliquam exercitationem odit quo eligendi. Facere, ullam.
+    Deleniti quia repellat cupiditate aut modi enim et necessitatibus optio, fuga corrupti eaque praesentium delectus nesciunt veritatis, quam non consectetur expedita debitis, ipsam tenetur natus qui aliquid. Vel, tempore atque?
+    Veniam quod libero pariatur possimus vitae fuga eaque excepturi molestias ipsum, debitis, eos voluptatibus aut optio unde adipisci perspiciatis et illo dolor! Dicta, at. Aperiam eaque tempore magni expedita unde.
+    Porro odio esse aspernatur animi exercitationem, officiis eum est sapiente, dolores hic voluptates accusantium. Veniam optio corrupti quam quibusdam quo. Esse quibusdam error laudantium sequi, vitae eligendi quos excepturi nesciunt.
+    Itaque perspiciatis voluptatem cumque sit atque voluptate porro alias, nostrum laudantium repellat sunt quos, repellendus aut officia assumenda ex laborum minus reiciendis, voluptatum suscipit est odio numquam! Possimus, repellat ducimus.
+    Ea ratione repellat assumenda magnam facilis quia nobis aperiam deleniti quis doloremque voluptates modi, eligendi illo tempora a accusamus perspiciatis quibusdam dolorum vero eaque odit reiciendis voluptas fugit optio. Ad.
+    Sit cum omnis reprehenderit natus perferendis velit voluptate ut officiis minus, animi laudantium adipisci deserunt saepe repellendus. Voluptas maxime itaque, placeat non veritatis quasi consequatur, est illum, rerum vero pariatur.
+    Atque placeat ipsam perferendis dolore. Quidem consequuntur doloremque similique adipisci? Minima in ad nisi tempora debitis doloribus, deserunt odio assumenda ullam optio mollitia nulla error repellendus harum fuga, rerum illum!
+    </p>
+    </div>
+</main>
