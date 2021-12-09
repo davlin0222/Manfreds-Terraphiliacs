bc2024a8da787ae536ee2379d04061d1e7d56232
DavLin <davlin@creatorise.com>
2021-12-09 16:27:08 +0100

    Reformat layout code

--- src/Layout/header.php
+++ src/Layout/header.php
@@ -1,16 +1,18 @@
 </head>
-  <body>
-    <header>
-      <nav>
-        <span class="menuButton">&equiv;</span>
-        <div class="home">
-          <img class="logo" src="../../images/pine.png" alt=" ">
-          <h1>Terraphiliacs</h1>
-        </div>
-        <div class="menu mobileHidden noTransition">
-          <a href="../../articles/">Articles</a>
-          <a href="#">Test2</a>
-          <a href="#">Test3</a>
-        </div>
-      </nav>
-    </header>
+    <body>
+        <header>
+            <nav>
+                <span class="menuButton">&equiv;</span>
+
+                <div class="home">
+                    <img class="logo" src="../../images/pine.png" alt=" ">
+                    <h1>Terraphiliacs</h1>
+                </div>
+
+                <div class="menu mobileHidden noTransition">
+                    <a href="/articles">Articles</a>
+                    <a href="#">Test2</a>
+                    <a href="#">Test3</a>
+                </div>
+            </nav>
+        </header>
