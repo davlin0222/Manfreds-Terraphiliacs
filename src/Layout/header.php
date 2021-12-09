5f4c176c64ce5b4a8fb6c04275f309b272603ae4
DavLin <davlin@creatorise.com>
2021-12-09 19:05:27 +0100

    Style menu for small screens

--- src/Layout/header.php
+++ src/Layout/header.php
@@ -7,10 +7,10 @@
             </a>
             
             <nav class="menu">
-                <a href="/">Home</a>
-                <a href="/articles">Articles</a>
-                <a href="/contact">Contact</a>
-                <a href="/about">About</a>
+                <a class="menu__item" href="/">Home</a>
+                <a class="menu__item" href="/articles">Articles</a>
+                <a class="menu__item" href="/contact">Contact</a>
+                <a class="menu__item" href="/about">About</a>
             </nav>
             
             <div class="menuButton">&equiv;</div>
